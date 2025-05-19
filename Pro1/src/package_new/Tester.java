3d1fb4265553ef4914c5023e84c0afa909762a4a
dipakpardeshi7 <dipakpardeshi7@gmail.com>
2025-05-19 19:31:33 +0530

    New Clone commit

--- Pro1/src/package_new/Tester.java
+++ Pro1/src/package_new/Tester.java
@@ -5,7 +5,9 @@ public class Tester {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
    System.out.println("Hello");
-   
+   System.out.println("Hello");
+   System.out.println("Hello");
+
 	}
 
 }
